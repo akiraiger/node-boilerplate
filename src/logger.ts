5433092ca97229a96ffd2a55441735255dc56c03
Fajar Alif <fajaralif@icloud.com>
2021-03-02 17:42:35 +0700

    fix: hooks and logger (#3)
    * fix: husky hooks
    
    * fix: winston logger format
    
    * style: formatting

--- src/logger.ts
+++ src/logger.ts
@@ -3,8 +3,10 @@ import winston from 'winston'
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
-    winston.format.json(),
-    winston.format.timestamp()
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    winston.format.simple()
   ),
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
