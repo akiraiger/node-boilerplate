d37a033a6f8abc4018464030ccc645feac333f7f
Fajar Alif <fajaralif@icloud.com>
2021-03-02 16:02:55 +0700

    feat: add prisma

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,35 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const router = express.Router()
 
 router.get('/health', (req, res) => {
-  res.send('Healthy')
+  res.json('Healthy')
+})
+
+router.get('/test', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+
+  res.json(allUsers)
 })
 
 export default router
