6c6d149ff9a17c39ce19e05285ea40393608f5b6
Fajar Alif <fajaralif@icloud.com>
2021-03-02 16:49:32 +0700

    feat: prisma orm (#2)
    * feat: add prisma
    
    * test: prisma query
    
    * chore: add mysql on github workflows
    
    * chore: migrate database on github workflows

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,41 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 const router = express.Router()
 
 router.get('/health', (req, res) => {
-  res.send('Healthy')
+  res.json({
+    health: 'Healthy',
+  })
+})
+
+router.get('/test', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+
+  await prisma.$disconnect()
+
+  res.json({
+    data: allUsers,
+  })
 })
 
 export default router
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -7,6 +7,22 @@ test('Health check', async () => {
     .get('/api/health')
     .expect(200)
     .then((response) => {
-      expect(response.text).toBe('Healthy')
+      const health = response.body.health
+
+      expect(health).toBe('Healthy')
+    })
+})
+
+test('Prisma query', async () => {
+  await supertest(app)
+    .get('/api/test')
+    .expect(200)
+    .then((response) => {
+      const data = response.body.data
+      const user = response.body.data[0]
+
+      expect(data.length).toBe(1)
+      expect(user.posts[0].title).toBe('Hello World')
+      expect(user.profile.bio).toBe('I like turtles')
     })
 })
